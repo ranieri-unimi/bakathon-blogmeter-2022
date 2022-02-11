808c388e82d304c1cb7ce2b97dcf13b1bee8c571
ranieri-unimi <51285505+ranieri-unimi@users.noreply.github.com>
2022-02-11 15:19:46 +0100

    bkp

---
+++ R/packages-manual.R
@@ -0,0 +1,27 @@
+install.packages('DT')
+install.packages('tidyverse')    # Meta - dplyr, ggplot2, purrr, tidyr, stringr, forcats
+install.packages('lubridate')    # date and time
+install.packages('timetk')       # Time series data wrangling, visualization and preprocessing
+install.packages('DataExplorer') # Data Viz
+install.packages('fs')           # Work with files
+install.packages('forecast')     # Auto ARIMA, ETS
+install.packages('prophet')      # FB Prophet
+
+install.packages('glmnet')       # Elastic Net
+install.packages('earth')        # Mars Regression Splines
+install.packages('kernlab')      # Support Vector Machine
+install.packages('knn')          # K-Nearest Neighbors
+install.packages('randomForest') # Random Forest
+install.packages('ranger')       # Random Forest
+install.packages('xgboost')      # Boosted Trees
+install.packages('Cubist')       # Cubist Rule-Based Algorithm
+install.packages('rules')        # Rule-based models ('required for Cubist')
+
+install.packages('tidymodels')         # Meta - workflows, parsnip, tune, dials, recipes, rsample, yardstick
+install.packages('modeltime')          # tidymodels time series extension
+install.packages('modeltime.resample') # tidymodels time series backtesting extension
+install.packages('modeltime.ensemble') # tidymodels time series ensembling extension
+install.packages('modeltime.h2o')      # tidymodels time series auto-ml with h2o extension
+
+install.packages('reticulate')         # Python interface
+install.packages('modeltime.gluonts')  # tidymodels interface to Amazon's deep learning algos
